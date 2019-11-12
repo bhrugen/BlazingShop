89ba2663c6220f6ae5e297061309e4fec8274ce4
Ben Spark <bhrugen1990@gmail.com>
2019-11-12 15:42:10 -0600

    Section 3 - Component Base

---
+++ BlazingShop/Pages/Learn Blazor/LearnBlazor.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Pages.Learn_Blazor
+{
+    public class LearnBlazorBase : ComponentBase
+    {
+        protected string name = "Spark";
+        protected string WelcomeText = "Time to learn Blazor!";
+
+        protected void getName()
+        {
+            name = "Blazor Learner";
+        }
+    }
+}
