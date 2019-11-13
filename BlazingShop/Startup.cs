2154a6f07c0aeb225bd30a2c70184fbea181a009
Ben Spark <bhrugen1990@gmail.com>
2019-11-13 11:09:16 -0600

    Section 4 - Register Category Service

--- BlazingShop/Startup.cs
+++ BlazingShop/Startup.cs
@@ -41,6 +41,7 @@ namespace BlazingShop
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<CategoryService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
