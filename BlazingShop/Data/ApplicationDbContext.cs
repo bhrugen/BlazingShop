73431de16632d162ff29a7579334944b74af6994
Ben Spark <bhrugen1990@gmail.com>
2019-11-13 13:45:44 -0600

    Section 5 - Create Product Model

--- BlazingShop/Data/ApplicationDbContext.cs
+++ BlazingShop/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace BlazingShop.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ BlazingShop/Data/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Data
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0,500)]
+        public double Price { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string ShadeColor { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
