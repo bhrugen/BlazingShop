fc2d91f8ad9a5c6b8b933ef356b2244a8c020f19
Ben Spark <bhrugen1990@gmail.com>
2019-11-13 11:01:32 -0600

    Section 4 - Category Service

---
+++ BlazingShop/Services/CategoryService.cs
@@ -0,0 +1,66 @@
+﻿using BlazingShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Services
+{
+    public class CategoryService
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Category GetCategory(int categoryId)
+        {
+            Category obj = new Category();
+            return _db.Categories.FirstOrDefault(u => u.Id == categoryId);
+        }
+
+        public List<Category> GetCategories(int categoryId)
+        {
+            
+            return _db.Categories.ToList();
+        }
+
+        public bool CreateCategory(Category objCategory)
+        {
+            _db.Categories.Add(objCategory);
+            _db.SaveChanges();
+            return true;
+        }
+        public bool UpdateCategory(Category objCategory)
+        {
+            var ExistingCategory = _db.Categories.FirstOrDefault(u => u.Id == objCategory.Id);
+            if(ExistingCategory != null)
+            {
+                ExistingCategory.Name = objCategory.Name;
+                _db.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteCategory(Category objCategory)
+        {
+            var ExistingCategory = _db.Categories.FirstOrDefault(u => u.Id == objCategory.Id);
+            if (ExistingCategory != null)
+            {
+                _db.Categories.Remove(ExistingCategory);
+                _db.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
