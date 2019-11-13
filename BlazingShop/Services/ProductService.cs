df588d3c6ea705e88a56992aefdc6ac7e05dd308
Ben Spark <bhrugen1990@gmail.com>
2019-11-13 13:54:29 -0600

    Section 5 - Product Service

---
+++ BlazingShop/Services/ProductService.cs
@@ -0,0 +1,74 @@
+﻿using BlazingShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingShop.Services
+{
+    public class ProductService
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Product GetProduct(int productId)
+        {
+            Product obj = new Product();
+            return _db.Products.Include(u => u.Category).FirstOrDefault(u => u.Id == productId);
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _db.Products.Include(u=>u.Category).ToList();
+        }
+        public List<Category> GetCategoryList()
+        {
+            return _db.Categories.ToList();
+        }
+
+        public bool CreateProduct(Product objProduct)
+        {
+            _db.Products.Add(objProduct);
+            _db.SaveChanges();
+            return true;
+        }
+        public bool UpdateProduct(Product objProduct)
+        {
+            var ExistingProduct = _db.Products.FirstOrDefault(u => u.Id == objProduct.Id);
+            if(ExistingProduct != null)
+            {
+                if (objProduct.Image == null)
+                {
+                    objProduct.Image = ExistingProduct.Image;
+                }
+                _db.Products.Update(objProduct);
+                _db.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteProduct(Product objProduct)
+        {
+            var ExistingProduct = _db.Products.FirstOrDefault(u => u.Id == objProduct.Id);
+            if (ExistingProduct != null)
+            {
+                _db.Products.Remove(ExistingProduct);
+                _db.SaveChanges();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
--- BlazingShop/Startup.cs
+++ BlazingShop/Startup.cs
@@ -42,6 +42,7 @@ namespace BlazingShop
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddSingleton<WeatherForecastService>();
             services.AddScoped<CategoryService>();
+            services.AddScoped<ProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
